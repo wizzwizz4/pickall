3e2208daaef34e9a8e7c54d1d77ba54f2f0c5951
wizzwizz4 <wizzwizz4@users.noreply.github.com>
2018-06-12 20:42:13 +0000

    Added basic functionality, but it doesn't work yet!
    To summarise: Everything in `types` works, but these actually exist in
    lots of places around the standard library (e.g. `functools`). By "works",
    I mean that everything in `types` that I've tested is found by pickle now.
    Their instances don't pickle properly; insufficient information is stored.
    
    I probably need to split this off into multiple files. This is going to be
    bigger than I thought.

--- pickall.py
+++ pickall.py
@@ -1,42 +1,83 @@
 import pickle
 import types
+import re
+import io
+import builtins
 
-# Add everything from pickle's globals...
+# Ensure that pickall has the same interface as pickle
+__all__ = pickle.__all__
+PickleError = pickle.PickleError
+PicklingError = pickle.PicklingError
+UnpicklingError = pickle.UnpicklingError
+
+# Add SHOUTY_VARIABLES from pickle's globals
 globals().update({k: v for k, v in vars(pickle).items()
-                  if not k[:2] == "__" == k[-2:]})  # ... except f"__{foo}__"
+                  if re.match("[A-Z][A-Z0-9_]+$", k)})
 
-def __duplicate(func):
+# Function duplication magic.
+class _DuplicateGlobals(dict):
+    """A chained dictionary implementation."""
+    def __init__(self, *globalss,
+                 set_globals=None, builtins=vars(builtins)):
+        if set_globals is None:
+            set_globals = globalss[0]
+        self._set_globals = set_globals
+        self._globalss = globalss + (builtins,)
+
+    def __getitem__(self, key):
+        for globals_ in self._globalss:
+            if key in globals_:
+                return globals_[key]
+        raise KeyError("Key {} not in any of the globalss.".format(key))
+
+    def __setitem__(self, key, value):
+        self._set_globals[key] = value
+
+def _duplicate(func, globals_ = _DuplicateGlobals(globals(), vars(pickle))):
+    # Replace the functions with versions that use this module's globals in
+    # preference to this function's globals.
     return types.FunctionType(
         func.__code__,
-        globals(),
+        globals_,
         func.__name__,
         func.__defaults__,
         func.__closure__
     )
 
-for __key in dir():
-    __value = globals()[__key]
+class _Pickler(pickle._Pickler):
+    # dispatch is a dictionary where the keys are the type of object
+    # and the values are save_x methods.
+    dispatch = pickle._Pickler.dispatch.copy()
+    del dispatch[types.FunctionType]
 
-    # Only want to work with what we've got from pickle.
-    if hasattr(__value, "__module__") and __value.__module__ != "pickle":
-        continue
-    
-    # Haven't used isinstance, just in case the pickle devs go crazy
-    # and hackishly subclass function to do strange stuff.
-    if type(__value) is types.FunctionType:
-        # Replace the functions with versions that use this module's globals
-        globals()[__key] = __duplicate(__value)
+    def save_type(self, obj, *args, **kwargs):
+        # save_type is called to save all instances of type.
+        # This includes the types.XyzType types.
+        
+        # I'm implementing this here, as opposed to in separate dispatches,
+        # because the types need to be implemented anyway and I'd just be
+        # reprogramming the existing object pickling code with a special case
+        # to avoid calling the dispatch for the class.
+        
+        if obj in map(vars(types).__getitem__, types.__all__):
+            name = next(k for k, v in vars(types).items()
+                        if v is obj)
+            
+            # I'm calling save_global here because I want this to work
+            # even if the pickle protocol optimises this; if I implemented this
+            # here using self.write it would be using protocol 4 max.
+            # This means that I have to also override whichmodule, which means
+            # that I have to _duplicate save_global.
+            return self.save_global(obj, name=name)
+        return super().save_type(obj, *args, **kwargs)
+    dispatch[type] = save_type  # Mustn't forget this!
 
-    if isinstance(__value, type):
-        # Subclass, but duplicate all methods.
-        # Current implementation may cause problems if pickle ever uses super.
-        # To correct this, the methods must have custom __get__s, iirc.
-        # Alternatively, it could be a sibling in the class hierarchy
-        # instead of a child.
-        globals()[__key] = type(
-            __key,
-            (__value,),
-            {k: __duplicate(v)
-             for k, v in vars(__value).items()
-             if type(v) is types.FunctionType}
-        )
+    # For explanation, see comments in save_type
+    save_global = _duplicate(pickle._Pickler.save_global)
+
+def whichmodule(obj, name):
+    if getattr(obj, '__module__', None) == 'builtins':
+        if (obj not in vars(builtins).values()
+            and obj in vars(types).values()):
+            return 'types'
+    return pickle.whichmodule(obj, name)
