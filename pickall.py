aa92ba5cfeb7306d51b08d640d00ba7f6d23b80d
wizzwizz4 <wizzwizz4@users.noreply.github.com>
2018-07-04 13:49:34 +0000

    Made tests cover all protocol versions, and fixed related bugs.

--- pickall.py
+++ pickall.py
@@ -6,6 +6,7 @@ import builtins
 import functools
 import copyreg
 import ctypes
+import sys
 
 # Ensure that pickall has the same interface as pickle
 __all__ = pickle.__all__
@@ -118,10 +119,30 @@ class _Pickler(pickle._Pickler):
     dispatch = pickle._Pickler.dispatch.copy()
     del dispatch[types.FunctionType]  # Don't treat it as a global
 
+    def save(self, obj, save_persistent_id=True, *args, **kwargs):
+        # dispatch_singletons is a dictionary where the keys are the object's
+        # id and the values are save_x methods.
+        if id(obj) not in self.dispatch_singletons:
+            return super().save(obj, save_persistent_id, *args, **kwargs)
+
+        # Housekeeping
+        self.framer.commit_frame()
+        if save_persistent_id:
+            pid = self.persistent_id(obj)  # Has priority over dispatch_x
+            if pid is not None:
+                self.save_pers(pid)
+                return
+        if obj in self.memo:  # Has priority over dispatch_x
+            self.write(self.get(self.memo[obj])[0])
+            return
+
+        self.dispatch_singletons[id(obj)](self)
+
     def save_function_call(self, func, *args):
         """Save a function and arguments, then call the function.
         
-        The first argument should be a callable.
+        The first argument should be a callable (convenience) or a tuple
+        as defined below.
         
         Each subsequent argument should be a tuple containing an int and
         some number of other items.
@@ -130,21 +151,32 @@ class _Pickler(pickle._Pickler):
         When the int is 1, the subsequent items of the tuple represent the
         arguments passed to save_function_call; it means that one of the
         arguments passed to the function is a function call.
-        When it's 2, the arguments should be a function, then an args tuple;
-        this should be a function that when called with *args pushes one item
-        onto the pickle stack. Alas, we all know that what _should be_, and
-        what _is_, are two different things. Just please don't write an
-        unmatched MARK.
+        When it's 2, the arguments should be a function, then an args tuple,
+        then an optional kwargs dictionary; this should be a function that when
+        called with *args and **kwargs pushes one item onto the pickle stack.
+        Alas, we all know that what _should be_, and what _is_, are two
+        different things. Just please don't write an unmatched MARK.
         
         More observant readers may have noticed that 1 is just a special case
         of 2; this is technically true but it's a lot more useful to be able
         to write nested function calls with only one level of nested tuples
-        instead of 2. Also, these readers may have noticed that magic numbers
-        as opposed to enums or PSEUDO_CONSTANTS have been used here. This is to
+        than of 2. Also, these readers may have noticed that magic numbers as
+        opposed to enums or PSEUDO_CONSTANTS have been used here. This is to
         discourage people from using what is basically a utility function for
         a last-resort hackish, hackish way of pickling.
         """
-        self.save(func)
+        modes = self.save_function_call.modes
+        
+        if callable(func):
+            self.save(func)
+        else:
+            mode, *arg = func
+            try:
+                mode_f = modes[mode]
+            except KeyError as e:
+                raise ValueError("{} is not a valid mode".format(mode)) from e
+            mode_f(self, *arg)
+            
         if not args:
             self.save_tuple(())  # No point reimplementing this logic
         
@@ -155,22 +187,11 @@ class _Pickler(pickle._Pickler):
             self.write(MARK)
 
         for mode, *arg in args:
-            if mode == 0:
-                # No special mode.
-                self.save(arg[0])
-                continue
-            if mode == 1:
-                self.save_function_call(*arg)
-                continue
-            if mode == 2:
-                # Structured like this to support the possibility of adding
-                # **kwargs in future; this is restricted by self.proto and I
-                # don't know what to do if it isn't supported so haven't
-                # implemented it.
-                f, f_args = arg
-                f(*f_args)
-                continue
-            raise ValueError("{} is not a valid mode".format(mode))
+            try:
+                mode_f = modes[mode]
+            except KeyError as e:
+                raise ValueError("{} is not a valid mode".format(mode)) from e
+            mode_f(self, *arg)
 
         if self.proto >= 2 and len(args) <= 3:
             # Write TUPLE1, TUPLE2 or TUPLE3
@@ -179,6 +200,11 @@ class _Pickler(pickle._Pickler):
             # End a variable-length tuple
             self.write(TUPLE)
         self.write(REDUCE)  # Call the function
+    save_function_call.modes = {
+        0: (lambda self, value: self.save(value)),  # No special mode.
+        1: (lambda self, *args: self.save_function_call(*args)),
+        2: (lambda self, f, args=(), kwargs={}: f(self, *args, **kwargs))
+    }
 
     def save_type(self, obj):
         # save_type is called to save all instances of type.
@@ -217,10 +243,12 @@ class _Pickler(pickle._Pickler):
                 pass
         
         func = types.FunctionType
+        pre_args = ()
         if self.proto >= 2:
             # __newobj__ is supported
             # Since __newobj__ is a function, this MUST not be used
             # unless __newobj__ will definitely not actually be pickled.
+            pre_args = (func,)
             func = __newobj__
 
         # Since __annotations__ and __kwdefaults__ are implemented as
@@ -243,14 +271,15 @@ class _Pickler(pickle._Pickler):
                 self.write(MARK)
             
         self.save_reduce(
-            func,
-            (types.FunctionType, obj.__code__, obj.__globals__,
+            func, pre_args +
+            (obj.__code__, obj.__globals__,
              # Afaik, function() copes with the optional arguments being
              # the default "empty" values.
              obj.__name__, obj.__defaults__, obj.__closure__),
-            state=vars(obj)
+            state=vars(obj),
             # Can't put __annotations__ and __kwdefaults__ here, since
             # they're descriptors that don't match a constructor argument.
+            obj=obj
         )
 
         if has_descriptors:
@@ -268,6 +297,26 @@ class _Pickler(pickle._Pickler):
                                 # because it's still the same object.
     dispatch[types.FunctionType] = save_function
 
+    def save_code(self, obj):
+        # This one's not much easier than function.
+        func = types.CodeType
+        pre_args = ()
+        if self.proto >= 2:
+            # code is required to pickle __newobj__, so it's very important
+            # not to use it if it's got a chance of actually being pickled.
+            pre_args = (func,)
+            func = __newobj__
+        self.save_reduce(
+            func, pre_args +
+            (obj.co_argcount, obj.co_kwonlyargcount,
+             obj.co_nlocals, obj.co_stacksize, obj.co_flags, obj.co_code,
+             obj.co_consts, obj.co_names, obj.co_varnames, obj.co_filename,
+             obj.co_name, obj.co_firstlineno, obj.co_lnotab, obj.co_freevars,
+             obj.co_cellvars),
+            obj=obj
+        )
+    dispatch[types.CodeType] = save_code
+
     def save_cell(self, obj):
         self.save_function_call(
             getattr, (1,
@@ -284,15 +333,6 @@ class _Pickler(pickle._Pickler):
     # dispatch_table is a registry of reduction functions
     dispatch_table = _ChainedDictionary(copyreg.dispatch_table)
 
-    # This one's MUCH easier than function.
-    dispatch_table[types.CodeType] = lambda c: (
-        __newobj__,
-        (types.CodeType, c.co_argcount, c.co_kwonlyargcount, c.co_nlocals,
-         c.co_stacksize, c.co_flags, c.co_code, c.co_consts, c.co_names,
-         c.co_varnames, c.co_filename, c.co_name, c.co_firstlineno,
-         c.co_lnotab, c.co_freevars, c.co_cellvars)
-    )
-
     # pickle functions from arbitrary CDLLs
     def _ctypes_FuncPtr(name):
         basename = name + '.'
@@ -301,6 +341,26 @@ class _Pickler(pickle._Pickler):
         return p
     dispatch_table[ctypes.pythonapi._FuncPtr] = _ctypes_FuncPtr('pythonapi')
     dispatch_table[ctypes.PyDLL] = lambda d: "pythonapi"
+
+    # dispatch_singletons is documented in save
+    # It's like dispatch, but for singletons and using ids as keys.
+    dispatch_singletons = {}
+    dispatch_singletons[id(cell)] = lambda s, d={}: s.save_function_call(
+        (1,
+            getattr,  # Takes three arguments; last one has default value None
+            (0, d),
+            (0, "__getitem__"),
+            (1,        # exec returns None, which needs to be discarded from
+                exec,  # the stack; placing it as the third argument to getattr
+                (0,    # serves to discard it
+                    # Golfed quite a bit.
+                    "cell=(lambda x:lambda:x)(0).__closure__[0].__class__"
+                ),
+                (0, d)  # Variable "cell" is put into this dictionary.
+            )
+        ),  # d.__getitem__
+        (0, "cell")
+    )
 
 def whichmodule(obj, name):
     location = resolve_location(obj)
--- test.py
+++ test.py
@@ -57,33 +57,54 @@ class FunctionPicklingTestCase(unittest.TestCase):
         @pickall._no_globals
         def original(a):
             return a * 2
+        
+        for protocol in range(pickle.HIGHEST_PROTOCOL + 1):
+            try:
+                del new_func
+            except NameError:
+                pass
+            
+            with self.subTest(protocol=protocol):
+                pickle_string = pickall.dumps(original, protocol=protocol)
+                new_func = pickle.loads(pickle_string)
+            try:
+                new_func
+            except NameError:
+                continue
 
-        pickle_string = pickall.dumps(original)
-        new_func = pickle.loads(pickle_string)
-
-        self.assertEqual(original(3),
-                         new_func(3))
+            self.assertEqual(original(3),
+                             new_func(3))
     
     def test_complex_isolated_function(self) -> tuple:
         @pickall._no_globals
         def original(a: int, b=12, *args, c=42, **kwargs):
             return a, b, c, args, kwargs
 
-        pickle_string = pickall.dumps(original)
-        new_func = pickle.loads(pickle_string)
+        for protocol in range(pickle.HIGHEST_PROTOCOL + 1):
+            try:
+                del new_func
+            except NameError:
+                pass
+            with self.subTest(protocol=protocol):
+                pickle_string = pickall.dumps(original, protocol=protocol)
+                new_func = pickle.loads(pickle_string)
+            try:
+                new_func
+            except NameError:
+                continue
+            
+            for attribute in ('__annotations__', '__kwdefaults__', '__name__'):
+                with self.subTest(attribute=attribute, protocol=protocol):
+                    self.assertEqual(getattr(original, attribute),
+                                     getattr(new_func, attribute))
 
-        for attribute in ('__annotations__', '__kwdefaults__', '__name__'):
-            with self.subTest(attribute=attribute):
-                self.assertEqual(getattr(original, attribute),
-                                 getattr(new_func, attribute))
-
-        for arguments in (((1, 2, 3), {'test': 4}),
-                          ((), {}),
-                          ((), {'a': 7})):
-            args, kwargs = arguments
-            with self.subTest(args=args, kwargs=kwargs):
-                self.assertEqual(original(1, 2, 3, test=4),
-                                 new_func(1, 2, 3, test=4))
+            for arguments in (((1, 2, 3), {'test': 4}),
+                              ((), {}),
+                              ((), {'a': 7})):
+                args, kwargs = arguments
+                with self.subTest(args=args, kwargs=kwargs, protocol=protocol):
+                    self.assertEqual(original(1, 2, 3, test=4),
+                                     new_func(1, 2, 3, test=4))
 
     def test_isolated_function_dictionary(self):
         @pickall._no_globals
@@ -93,33 +114,53 @@ class FunctionPicklingTestCase(unittest.TestCase):
         original.b = "this is b"
         original.c = ("this is a complex", "value for c")
 
-        pickle_string = pickall.dumps(original)
-        new_func = pickle.loads(pickle_string)
+        for protocol in range(pickle.HIGHEST_PROTOCOL + 1):
+            try:
+                del new_func
+            except NameError:
+                pass
+            with self.subTest(protocol=protocol):
+                pickle_string = pickall.dumps(original, protocol=protocol)
+                new_func = pickle.loads(pickle_string)
+            try:
+                new_func
+            except NameError:
+                continue
 
-        for attribute in 'abc':
-            with self.subTest(attribute=attribute):
-                self.assertEqual(getattr(original, attribute),
-                                 getattr(new_func, attribute))
+            for attribute in 'abc':
+                with self.subTest(attribute=attribute, protocol=protocol):
+                    self.assertEqual(getattr(original, attribute),
+                                     getattr(new_func, attribute))
 
     def test_isolated_closure(self):
-        def wrapper():
-            x = 0
-            @pickall._no_globals
-            def original():
-                nonlocal x
-                x += 1
-                return x, y
-            y = 24
-            return original
-        original = wrapper()
+        for protocol in range(pickle.HIGHEST_PROTOCOL + 1):
+            def wrapper():
+                x = 0
+                @pickall._no_globals
+                def original():
+                    nonlocal x
+                    x += 1
+                    return x, y
+                y = 24
+                return original
+            original = wrapper()
 
-        pickle_string = pickall.dumps(original)
-        new_func = pickle.loads(pickle_string)
+            try:
+                del new_func
+            except NameError:
+                pass
+            with self.subTest(protocol=protocol):
+                pickle_string = pickall.dumps(original, protocol=protocol)
+                new_func = pickle.loads(pickle_string)
+            try:
+                new_func
+            except NameError:
+                continue
 
-        for i in range(100):
-            with self.subTest(call_n=i):
-                self.assertEqual(original(),
-                                 new_func())
+            for i in range(100):
+                with self.subTest(call_n=i, protocol=protocol):
+                    self.assertEqual(original(),
+                                     new_func())
 
 # Undocumented
 class _duplicateTestCase(unittest.TestCase):
